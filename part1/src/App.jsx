c121c39291ae2cffcb4727c1cca36cca667ba874
Denese Joyce S. Rodriguez <denese.joyce.rodriguez@gmail.com>
2026-09-22 18:45:22 +0800

    1.3: course information step 3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,22 +4,24 @@ const Header = (props) => {
 
 const Part = (props) => {
   return (
-    <p>{props.name} {props.units}</p>
+    <p>{props.part.name} {props.part.units}</p>
   )
 }
 
 const Content = (props) => {
   return (
     <div>
-      <Part name={props.part1} units={props.units1} />
-      <Part name={props.part2} units={props.units2} />
-      <Part name={props.part3} units={props.units3} />
+      <Part part={props.part1} />
+      <Part part={props.part2} />
+      <Part part={props.part3} />
     </div>
   )
 }
 
 const Total = (props) => {
-  return <p>Number of units {props.total}</p>
+  return (
+    <p>Number of units {props.part1.units + props.part2.units + props.part3.units}</p>
+  )
 }
 
 const Footer = (props) => {
@@ -33,32 +35,28 @@ const Footer = (props) => {
 const App = () => {
   const course = 'Industry Elective'
 
-  const part1 = 'Information Management 2'
-  const units1 = 3
+  const part1 = {
+    name: 'Information Management 2',
+    units: 3
+  }
 
-  const part2 = 'Data Analytics 1'
-  const units2 = 3
+  const part2 = {
+    name: 'Data Analytics 1',
+    units: 3
+  }
 
-  const part3 = 'Project Management for IT'
-  const units3 = 3
+  const part3 = {
+    name: 'Project Management for IT',
+    units: 3
+  }
 
   const footerInfo = 'Denese Joyce S. Rodriguez - CSIT340 - G6'
 
   return (
     <div>
       <Header course={course} />
-
-      <Content
-        part1={part1}
-        units1={units1}
-        part2={part2}
-        units2={units2}
-        part3={part3}
-        units3={units3}
-      />
-
-      <Total total={units1 + units2 + units3} />
-
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total part1={part1} part2={part2} part3={part3} />
       <Footer info={footerInfo} />
     </div>
   )
